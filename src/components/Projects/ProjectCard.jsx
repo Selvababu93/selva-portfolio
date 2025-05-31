01c92cfb2b51a1f27cdd1304748b019e9c2494aa
Selvababu93 <Selvababu93@gmail.com>
2025-05-31 10:46:59 +0400

    added frist project

--- src/components/Projects/ProjectCard.jsx
+++ src/components/Projects/ProjectCard.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 
-const PaojectCard = ({projectImg, title, main}) => {
+const ProjectCard = ({projectImg, title, main}) => {
   return (
     <div className="p-3 md:p-6 flex flex-col w-80 bg-[#0c0e19] shadow-x1 shadow-slate-900 rounded-2xl">
         <img src={projectImg} alt="" />
@@ -13,12 +13,12 @@ const PaojectCard = ({projectImg, title, main}) => {
         </p>
         <div className="mt-2 p-2 md:p-4 flex gap-2 md:gap-4">
             <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85
-            duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697">
+            duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
                     Demo
             </button>
 
             <button className="md:mt-10 text-white py-2 px-3 text-sm md:text-lg md:py-2 md:px-4 hover:opacity-85
-            duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697">
+            duration-300 hover:scale-105 font-semibold rounded-3xl bg-[#465697]">
                     Source Code
             </button>
         </div>
@@ -26,4 +26,4 @@ const PaojectCard = ({projectImg, title, main}) => {
   )
 }
 
-export default PaojectCard
+export default ProjectCard
--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,33 +1,41 @@
-import React from 'react'
-import ProjectCard from "./ProjectCard"
-import usbRelay from "../../assets/pcbboard.png"
-import thumbnailImg from "../../assets/photographer.png"
-import codingImg from "../../assets/coder.png"
+import React from "react";
+import ProjectCard from "./ProjectCard";
+import usbRelay from "../../assets/pcbboard.png";
+import thumbnailImg from "../../assets/photographer.png";
+import codingImg from "../../assets/coder.png";
 const Projects = () => {
   return (
     <div id="Projects" className="p-10 md:p-24 text-white">
-        <h1 className="text-2xl md:text-4xl text-white font-bold">
-            Projects
-        </h1>
-                        {/* Project Details */}
-        <div className="py-12 px-8 flex flex-wrap gap-5">
-                        {/* Project cards */}
-            <ProjectCard projectImg={usbRelay} title={"Usb Relay"} main={`USB relay modules with GPIOs and analog
+      <h1 className="text-2xl md:text-4xl text-white font-bold">Projects</h1>
+      {/* Project Details */}
+      <div className="py-12 px-8 flex flex-wrap gap-5">
+        {/* Project cards */}
+        <ProjectCard
+          projectImg={usbRelay}
+          title={"Usb Relay"}
+          main={`USB relay modules with GPIOs and analog
                 inputs offer a versatile solution for
-                 controlling and monitoring various devices.`}/>
+                 controlling and monitoring various devices.`}
+        />
 
-                        {/* Project cards */}
-            <ProjectCard projectImg={thumbnailImg} title={"Usb Relay"} main={`USB relay modules with GPIOs and analog
-                inputs offer a versatile solution for
-                 controlling and monitoring various devices.`}/>
+        {/* Project cards */}
+        <ProjectCard
+          projectImg={videoThumbnailBackup}
+          title={"🎞️ Video Thumbnail & Backup Tool"}
+          main={`A desktop tool that auto-generates video thumbnails, embeds them using FFmpeg, and organizes backups. Built with PySide6, MoviePy & Watchdog.`}
+        />
 
-                        {/* Project cards */}
-            <ProjectCard projectImg={codingImg} title={"Usb Relay"} main={`USB relay modules with GPIOs and analog
+        {/* Project cards */}
+        <ProjectCard
+          projectImg={codingImg}
+          title={"Usb Relay"}
+          main={`USB relay modules with GPIOs and analog
                 inputs offer a versatile solution for
-                 controlling and monitoring various devices.`}/>
-        </div>
+                 controlling and monitoring various devices.`}
+        />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
